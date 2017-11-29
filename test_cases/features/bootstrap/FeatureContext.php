b0cbc96f2d1b719f026b8295bd25d89f3ad8b7ab
jsandov1 <jsandov1@jsandov1s-mbp.global.tektronix.net>
2017-11-28 16:18:45 -0800

    Added test cases, and custom contexts

---
+++ test_cases/features/bootstrap/FeatureContext.php
@@ -0,0 +1,31 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
+
+
+// Use Assert Functions as flat functions ... ie ->  assertContains($string,$this-output,)
+require_once __DIR__.'/../../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+}
